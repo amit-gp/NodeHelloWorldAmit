58c42aa08323da30dab072b7ddf34472521f7c9e
DESKTOP-MOKBNTP\Amit Kumar <theamit97@gmail.com>
2017-07-12 13:44:31 +0530

    Adding ckeditor to workflow

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var http = require('http');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
 
 http.createServer(function(request, response){
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Wrold\n');
+  var hello = require('url').parse(request.url, true).query.hello;
+  if(hello === undefined){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello Wrold\n');
+  }
+  else {
+    fs.readFile('./res/index.html', (err, data) {
+      if(err){
+        console.log("ERROR reading file !!!!");
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end('404 File hello not found !');
+      }
+      else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+      }
+    });
+
+  }
 }).listen(port);
 
 console.log("Listening on PORT : " + port + " .......");
